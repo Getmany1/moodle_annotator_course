0c8679bbf531b47add928db5189a07afbc3fb815
getmany1@aalto.fi <getmany1@aalto.fi>
2020-10-02 16:53:06 +0300

    another commit..

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 import requests
+import os
 import urllib3
+from utils import *
 from my_cookies import cookies, headers # personal cookies from moodle (convert curl to python with https://curl.trillworks.com/)
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+#Parameters
+quiz = 'quiz.txt' # txt file which contains links for student audio responses
+parent_dir = os.path.join('moodle_quiz')
 
-def write2disk(file, name):
-    with open(f'{name}.ogg', 'wb') as f:
-        f.write(file)
+dict_userid_username = getdict_userid_username()
 
+dict_q, dict_task_prompt, dict_question_prompt = getdicts_question_task()
 
-with open('Suullisenkielitaidonkoe,suomi-steps.txt', 'r') as file:
-    for line in file:
+make_dirs(dict_task_prompt, dict_question_prompt)
+
+#def parse_quiz(quiz, dict_userid_username):
+with open(quiz, 'r') as f:
+    for line in f:
         if line.startswith('save-file'):
             _, url, _, userpath = line.strip().split(' ')
-            usr, qnum = userpath.split('/')[0], userpath.split('/')[1]
-            fname  = f"{usr}_{qnum}"
+            userid, qnum = int(userpath.split('/')[0].split('-')[2]), int(userpath.split('/')[1].split('-')[0][1:])
+            username = dict_userid_username[userid]
+            question_num = dict_q[qnum]
+            task_num = int(question_num[:-1])
+            os.mkdir(os.path.join(parent_dir, str(task_num), question_num, username))
             r = requests.get(url, allow_redirects=True, headers=headers, cookies=cookies, verify=False)
-            write2disk(r.content, fname)
-            print(url, userpath)
-
-            # break
+            with open(os.path.join(parent_dir, str(task_num), question_num, username, 'recording.ogg'), 'wb') as f:
+                f.write(r.content)
---
+++ utils.py
@@ -0,0 +1,66 @@
+from xml.dom.minidom import parse
+import os
+import re
+
+def natural_sort_key(s):
+    return [int(text) if text.isdigit() else text.lower()
+            for text in re.split(re.compile('([0-9]+)'), s)]
+
+def getdict_userid_username():
+    dict_userid_username = {}
+    userxml = os.path.join("backup","users.xml")
+    docs = parse(userxml)
+    users = docs.getElementsByTagName("user")
+    for user in users:
+        userid = int(user.getAttribute("id"))
+        username = user.getElementsByTagName("username")[0].firstChild.nodeValue
+        dict_userid_username[userid] = username
+    return dict_userid_username
+    
+def getdicts_question_task():
+    """
+    Returns:
+    dict_q: dictionary from question number in quiz.txt (1,2,3,4,5...) to question number in moodle (1a,1b,2a,2b,2c...)
+    dict_task_prompt: dictionary from task number in moodle (1,2,3,4...) to corresponding task prompt
+    dict_question_prompt: dictionary from question number in moodle (1a,3c,6f...) to corresponding prompt
+    """
+    dict_q = {}
+    dict_task_prompt = {}
+    dict_question_prompt = {}
+
+    question_list = []
+    qxml = os.path.join("backup","questions.xml")
+    docs = parse(qxml)
+    tasks = docs.getElementsByTagName("question_category")
+    for task in tasks:
+        name = task.getElementsByTagName("name")[0].firstChild.nodeValue
+        if name.split(' ')[0] == "Tehtävä":
+            tasknum = int(name.split(' ')[1])
+            questions = task.getElementsByTagName("question")
+            for q in questions:
+                prompt = q.getElementsByTagName("questiontext")[0].firstChild.nodeValue # task_prompt.txt / prompt.txt
+                q_name = q.getElementsByTagName("name")[0].firstChild.nodeValue
+                # search for @@PLUGINFILE@@, which contains the name of prompt audio or image
+                if q_name.split(' ')[1].isdigit(): # Tehtävä X (1,2...)
+                    dict_task_prompt[tasknum] = prompt
+                else: # Tehtävä Xx (1a,2f...)
+                    task_question = q_name.split(' ')[1]
+                    dict_question_prompt[task_question] = prompt
+                    question_list.append(task_question)
+    question_list.sort(key=natural_sort_key)
+    for i in range(len(question_list)):
+        dict_q[i+1] = question_list[i]
+    return dict_q, dict_task_prompt, dict_question_prompt
+
+def make_dirs(dict_task_prompt, dict_question_prompt):
+    parent_dir = os.path.join('moodle_quiz')
+    os.mkdir(parent_dir)
+    for tasknum in dict_task_prompt.keys():
+        os.mkdir(os.path.join(parent_dir,str(tasknum)))
+        with open(os.path.join(parent_dir,str(tasknum),'task_prompt.txt'), 'w', encoding='utf-8') as f:
+            f.write(dict_task_prompt[tasknum])
+        for qnum in dict_question_prompt.keys():
+            if tasknum == int(qnum[:-1]):
+                os.mkdir(os.path.join(parent_dir,str(tasknum),qnum))
+                with open(os.path.join(parent_dir,str(tasknum),qnum,'prompt.txt'), 'w', encoding='utf-8') as f:
+                    f.write(dict_question_prompt[qnum])
